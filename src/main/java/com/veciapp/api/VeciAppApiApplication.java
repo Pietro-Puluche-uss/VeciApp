edc3ddab7ab11ac1035151a3218d2d4f230b66ed
Pietro-Puluche-uss <pespejopietrora@uss.edu.pe>
2026-05-04 16:14:56 -0500

    fix: remove default security user warnings

--- src/main/java/com/veciapp/api/VeciAppApiApplication.java
+++ src/main/java/com/veciapp/api/VeciAppApiApplication.java
@@ -2,12 +2,12 @@ package com.veciapp.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class VeciAppApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VeciAppApiApplication.class, args);
     }
 }
-
